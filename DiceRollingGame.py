c8d148ad9a21a39674156b62e889b9c160650690
Ayaan Afsar <itsayaanafsar@gmail.com>
2026-01-29 18:59:09 +0530

    Update DiceRollingGame.py

--- DiceRollingGame.py
+++ DiceRollingGame.py
@@ -3,12 +3,12 @@ count = 0
 while True:
     choice = input("Roll the dice? (y/n): ").lower()
     if choice == "y":
-        time = int(input("How many times do you want to roll the dice? "))
-        for i in range(time):
+        rolls = int(input("How many times do you want to roll the dice? "))
+        for i in range(rolls):
             dice1 = random.randint(1, 6)
             dice2 = random.randint(1, 6)
             print(f"({dice1}, {dice2})")
-        count += time
+        count += rolls
         print(f"Total rolls: {count}")
         
     elif choice == "n":
@@ -17,3 +17,4 @@ while True:
         break
     else:
         print("Invalid input. Please enter 'y' or 'n'.")
+
